e6f04a2b7afb322a89ee0cb12d34afd3c168ac3b
Noble <ruzibizanoble@gmail.com>
2022-03-10 16:26:37 -0800

    Task five

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * My Hello, printf
+ * Return: return 1 after executing a program
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
