7066c9172c4d600c09646358ec9b06410b3343ae
Noble <ruzibizanoble@gmail.com>
2022-04-01 15:22:49 -0700

    My First Commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Name of the program printed
+ * @argc: Command line arguments
+ * @argv: Array name
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+
+	return (0);
+}
