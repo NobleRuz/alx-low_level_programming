4a449e201eee299f9bd5b4e01855fdfdee343d20
Noble <ruzibizanoble@gmail.com>
2022-04-21 19:42:51 -0700

    Linked List Committed

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,22 +7,18 @@
  *
  * Return: Number of nodes in h.
  */
-
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
-
-	while (h)
-	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-
-		else
-			printf("[%d] %s\n", h->len, h->str);
-
-		nodes++;
-		h = h->next;
-	}
-
-	return (nodes);
+size_t nodes;
+nodes = 0;
+while (h != NULL)
+{
+if (h->str == NULL)
+printf("[%d] %s\n", 0, "(nil)");
+else
+printf("[%d] %s\n", h->len, h->str);
+h = h->next;
+nodes++;
+}
+return (nodes);
 }
