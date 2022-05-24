dff27c39ed46bfe344301ec96d4c9d9c00f9efc0
Noble <ruzibizanoble@gmail.com>
2022-05-24 05:41:20 -0700

    My Commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,24 +1,34 @@
-#include <stdio.h>
-#include "../main.h"
+#include "main.h"
 
 /**
- * main: check the code.
- *
- * Return: Always 0.
+ * binary_to_uint  - converts a binary number to an unsigned int
+ * @b:  pointing to a string of 0 and 1 chars
+ * Return:   converted number or 0 if b is NULL
  */
-int main(void)
+unsigned int binary_to_uint(const char *b)
 {
-    unsigned int n;
 
-    n = binary_to_uint("1");
-    printf("%u\n", n);
-    n = binary_to_uint("101");
-    printf("%u\n", n);
-    n = binary_to_uint("1e01");
-    printf("%u\n", n);
-    n = binary_to_uint("1100010");
-    printf("%u\n", n);
-    n = binary_to_uint("0000000000000000000110010010");
-    printf("%u\n", n);
-    return (0);
+	unsigned int num = 0;
+	int i;
+
+	/* if string is empty return 0 */
+	if (b == NULL)
+		return (0);
+
+	/* return if the value is different to 0 or 1*/
+	for (i = 0; b[i] != '\0'; i++)
+		if (b[i]  != '0' && b[i] != '1')
+			return (0);
+
+	/* converter array the number binary to  int */
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		num *= 2;
+		if (b[i] == '1')
+			num += 1;
+	}
+	return (num);
+
+
+
 }
