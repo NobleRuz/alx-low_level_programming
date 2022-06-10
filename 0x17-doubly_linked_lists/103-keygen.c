2249a15916e4c6e58906e6fcda6f11dd71c4b765
Noble <ruzibizanoble@gmail.com>
2022-06-09 17:08:05 -0700

    Keygen

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -3,9 +3,9 @@
 #include <stdlib.h>
 
 /**
- * main: Generates password for crackme5
- * @argc: Number of arguments 
- * @argv: Arguments passed to main
+ * main - generate a key depending on a username for crackme5
+ * @argc: number of arguments passed
+ * @argv: arguments passed to main
  *
  * Return: 0 on success, 1 on error
  */
